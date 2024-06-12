5b41048ade96e5ec6d28101f23dceea31a06643d
이연승 <leeys951006@gmail.com>
2024-06-12 14:46:02 +0900

    5

--- teamplate.js
+++ teamplate.js
@@ -1,11 +1,23 @@
+const day = require('./date.js');
+// console.log(day);
+
 const tagComponent = (tagName, textNode) => {
   return `<${tagName}>${textNode}</${tagName}>`;
 };
 
 const container = () => {
+  const today = (textNode) => {
+    if (textNode === day) {
+      return `${tagComponent('h1', '오늘의 날짜와 같음')}`;
+    } else {
+      return `${tagComponent('h1', '오늘의 날짜와 다름')}`;
+    }
+  };
+
   return `
-    ${tagComponent('header', '헤더부분')}
-    ${tagComponent('main', '메인부분')}
+    ${today(12)}
+    ${today(13)}
     ${tagComponent('footer', '푸터부분')}
   `;
 };
+console.log(container());
