e6fc46c25dc63cd037dfd9cda4364f9ce6e2c89c
이연승 <leeys951006@gmail.com>
2024-06-12 15:52:56 +0900

    work-3

--- work-3.js
+++ work-3.js
@@ -1,2 +1,23 @@
 const names = require('./names.js');
-console.log(names);
+// console.log(names);
+
+// 내용 들어갈 틀 만들어주기
+const a = (family, FName) => {
+  return `<${family}>${FName}`;
+};
+
+const b = () => {
+  const c = (name) => {
+    if (name === '이연승') {
+      return `${a('본인', '본인입니다.')}`;
+    } else {
+      return `${a('가족', '가족입니다.')}`;
+    }
+  };
+
+  return `
+  ${c('정호연')}
+  `;
+};
+
+console.log(b());
