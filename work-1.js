ac867cb723be37abe5e45bfe05a48cd5dd0a617a
이연승 <leeys951006@gmail.com>
2024-06-12 10:41:48 +0900

    2(정말 많이보는 패턴)

---
+++ work-1.js
@@ -0,0 +1,15 @@
+const basicData = () => {
+  return {
+    name: '이연승',
+    age: 30,
+    job: '무직',
+  };
+};
+
+const component = () => {
+  let obj = basicData();
+
+  const decision = () => {};
+
+  return `<html></html>`;
+};
